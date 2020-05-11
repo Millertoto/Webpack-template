294165e4a59e225e350664c69267ad9414f587be
David N. Miller <48084926+Millertoto@users.noreply.github.com>
2020-05-11 10:25:24 +0200

    bookvue

--- src/js/index.ts
+++ src/js/index.ts
@@ -1,12 +1,56 @@
-interface Person {
-    firstName: string;
-    lastName: string;
+import axios, {
+    AxiosResponse,
+    AxiosError
+} from "../../node_modules/axios/index"
+
+interface IBook {
+    isbn13:string,
+    titel:string,
+    forfatter:string,
+    sidetal:number
 }
 
-function greeter(person: Person): string {
-    return "Hello, " + person.firstName + " " + person.lastName;
-}
-let user: Person = { firstName: "John", lastName: "Doe" };
+let baseUri: string = "http://localhost:5000/api/bog"
 
-let element: HTMLDivElement = <HTMLDivElement> document.getElementById("content");
-element.innerHTML = greeter(user);
+new Vue({
+    el: "#app",
+    data:{
+        Books:[],
+        errors:[],
+        deleteId: 0,
+        deleteMessage: "",
+        formData: {isbn13:"", titel:"",forfatter:"", sidetal:0}
+    },
+    methods:{
+        getAllBooks(){
+            axios.get<IBook[]>(baseUri)
+            .then((Response: AxiosResponse<IBook[]>)=>{
+                this.Books = Response.data
+            })
+            .catch((error:AxiosError) =>{
+                console.log(error.message)
+            })
+        },
+        deletebook(deleteId: number){
+            let uri: string = baseUri + "/" + deleteId
+            axios.delete<void>(uri)
+            .then((response: AxiosResponse)=>{
+                this.deleteMessage = response.status + " " + response.statusText
+                this.getAllBooks()
+            })
+            .catch((error:AxiosError) =>{
+                console.log(error.message)
+            })
+        },
+        addbook(){
+            axios.post<IBook>(baseUri, this.formData)
+            .then((response: AxiosResponse) =>{
+                let message: string = "response" + response.status + " " + response.statusText
+                this.getAllBooks()
+            })
+            .catch((error:AxiosError) =>{
+                console.log(error.message)
+            })
+        }
+    }
+})
